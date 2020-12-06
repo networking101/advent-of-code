0c06e3a106ea69cdb495affe4b2f10c69d041361
Michael Kowpak <kowpak14@gmail.com>
2020-12-06 16:44:43 -0500

    2020 day 6

--- 2020/custom_customs.py
+++ 2020/custom_customs.py
@@ -1,49 +1,38 @@
 with open("input.txt", "r") as fp:
-	lines = [line.strip() for line in fp]
-
-print(lines)
+    lines = [line.strip() for line in fp]
 
 cnt = 0
 tl = []
 
+lines.append("")
+
+#ans = "silver"
+ans = "gold"
+
 for line in lines:
-	
-	"""
-	if not line:
-		cnt += len(tl)
-		tl = []
-		continue
-	"""
-	if not line:
-		for i in tl[0]:
-			cond = True
-			for j in tl:
-				if i not in j:
-					cond = False
-			if cond == True:
-				cnt += 1
-		tl = []
-		print(cnt)
-		continue	
 
-	"""
-	for i in line:
-		if i not in tl:
-			tl.append(i)
-	"""
+    if ans == "silver":
+        if not line:
+            cnt += len(tl)
+            tl = []
+            continue
 
-	tl.append(line)
+        for i in line:
+            if i not in tl:
+                tl.append(i)
 
+    if ans == "gold":
+        if not line:
+            for i in tl[0]:
+                cond = True
+                for j in tl:
+                    if i not in j:
+                        cond = False
+                if cond == True:
+                    cnt += 1
+            tl = []
+            continue
 
-#cnt += len(tl)
-
-for i in tl[0]:
-	cond = True
-	for j in tl:
-		if i not in j:
-			cond = False
-	if cond == True:
-		cnt += 1
-print(cnt)
+        tl.append(line)
 
 print(cnt)
