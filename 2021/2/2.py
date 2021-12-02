d94c51aec4716b6b23ec252e8716e6b09a4510dd
networking101 <kowpak14@gmail.com>
2021-12-02 01:02:43 -0500

    2021 day 2

--- 2021/2/2.py
+++ 2021/2/2.py
@@ -1,2 +1,36 @@
 with open("input", "r") as fp:
-    lines = [int(line.strip()) for line in fp]
+    lines = [line.strip() for line in fp]
+
+# Silver
+forward = 0
+depth = 0
+
+for line in lines:
+    direction, magnitude = line.split(" ")
+    magnitude = int(magnitude)
+    if direction == "forward":
+        forward += magnitude
+    elif direction == "down":
+        depth += magnitude
+    elif direction == "up":
+        depth -= magnitude
+
+print(forward * depth)
+
+# Gold
+forward = 0
+depth = 0
+aim = 0
+
+for line in lines:
+    direction, magnitude = line.split(" ")
+    magnitude = int(magnitude)
+    if direction == "forward":
+        forward += magnitude
+        depth += aim * magnitude
+    elif direction == "down":
+        aim += magnitude
+    elif direction == "up":
+        aim -= magnitude
+
+print(forward * depth)
