93a23d1d2f2b4d275b9ea7df04e29c323055fb85
networking101 <kowpak14@gmail.com>
2021-12-02 00:56:48 -0500

    2021 day 2

--- 2021/1/1.py
+++ 2021/1/1.py
@@ -1,32 +1,5 @@
-from copy import deepcopy
-
 with open("input", "r") as fp:
     lines = [int(line.strip()) for line in fp]
 
-backup = deepcopy(lines)
-
-res = 0
-
-# Silver
-index = lines.pop(0)
-for line in lines:
-    if line > index:
-        res += 1
-    index = line
-
-print(res)
-
-# Gold
-lines = backup
-
-res = 0
-index = sum(lines[:3])
-temp = index
-for i in range(3, len(lines)):
-    temp += lines[i]
-    temp -= lines[i-3]
-    if temp > index:
-        res += 1
-    index = temp
-
-print(res)
+print(sum(x < y for x, y in zip(lines, lines[1:])))
+print(sum(x < y for x, y in zip(lines, lines[3:])))
---
+++ 2021/2/solve2.asm
@@ -0,0 +1,103 @@
+; solve2.asm
+; binary boarding solution part 2
+section         .data
+
+section         .bss
+    bufStart        resq    1
+    indexSize       resq    1
+
+	forward			resq	1
+	depth			resq	1
+	aim				resq	1
+
+extern atoi
+
+section         .text
+global solve2
+solve2:
+	push rbp
+	push rbx
+	push r15
+	push r14
+	push r13
+	push r12
+    mov rbp, rsp
+
+main:
+    mov qword [bufStart], rdi
+	mov r12, rdi
+	mov rcx, rsi
+l1:
+	cmp byte [rdi], 0xa
+	jne nonewline
+	mov byte [rdi], 0
+	push r12
+	mov r12, rdi
+	inc r12
+	inc qword [indexSize]
+
+nonewline:
+	inc rdi
+	loop l1
+
+position:			; calculate position
+	mov qword [forward], 0
+	mov qword [depth], 0
+	mov r14, 0
+	mov r13, 1
+l2:					; loop through each index and check if the command is "forward", "down", or "up"
+	mov r12, rbp
+	mov rax, 8
+	mul r13
+	sub r12, rax
+	mov r14, r12
+	mov r12, qword [r12]
+	cmp dword [r12], 0x77726f66				; forward
+	je fwd
+	cmp dword [r12], 0x6e776f64				; down
+	je down
+	cmp word [r12], 0x7075					; up
+	je up
+
+fwd:
+	add r12, 8
+	mov rdi, r12
+	call atoi
+	add qword [forward], rax
+	imul rax, qword [aim]
+	add qword [depth], rax
+	jmp next
+
+down:
+	add r12, 5
+	mov rdi, r12
+	call atoi
+	add qword [aim], rax
+	jmp next
+
+up:
+	add r12, 3
+	mov rdi, r12
+	call atoi
+	sub qword [aim], rax
+	jmp next
+
+next:			; move to next index
+	inc r13
+	cmp r14, rsp
+	jne l2
+
+done:			; multiply forward position by depth and move into return value
+	mov rax, qword [forward]
+	mov rbx, qword [depth]
+	mul rbx
+
+fin:
+    mov rsp, rbp
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	pop rbx
+	pop rbp
+    ret
