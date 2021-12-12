591b79cdd8b4f7359606257d49f5760a5dd35452
networking101 <kowpak14@gmail.com>
2021-12-12 17:47:55 -0500

    2021 day 11

--- 2021/11/11.py
+++ 2021/11/11.py
@@ -6,40 +6,10 @@ with open("input", "r") as fp:
 flashes = 0
 backupGrid = deepcopy(grid)
 
-for _ in range(100):
-    queue = []
-    for y in range(10):
-        for x in range(10):
-            grid[y][x] += 1
-            if grid[y][x] == 10:
-                queue.append([y, x])
-                flashes += 1
-
-    while queue:
-        y, x = queue.pop(0)
-
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                ty = y+i
-                tx = x+j
-                if ty >= 0 and tx >= 0 and ty <= 9 and tx <= 9:
-                    grid[ty][tx] += 1
-                    if grid[ty][tx] == 10:
-                        queue.append([ty, tx])
-                        flashes += 1
-
-    for y in range(10):
-        for x in range(10):
-            if grid[y][x] > 9:
-                grid[y][x] = 0
-
-print(flashes)
-
-# Gold
-grid = backupGrid
 count = 0
+loopCount = 0
 while True:
-    count += 1
+    loopCount += 1
     queue = []
     for y in range(10):
         for x in range(10):
@@ -69,8 +39,10 @@ while True:
             if grid[y][x] != 0:
                 check = False
     
-    
     if check:
         break
-    
-print(count)
+
+    if loopCount == 100:
+        print(flashes)
+
+print(loopCount)
