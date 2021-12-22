039d1557ce30ab5db47502a6febd5392174447f2
networking101 <kowpak14@gmail.com>
2021-12-21 21:38:32 -0500

    2021 day 18

--- 2021/18/18.py
+++ 2021/18/18.py
@@ -45,72 +45,99 @@ def fillPlaces(places, position, curr):
 
     return
 
-def followUpLeft(places, left):
-    curr = left[:-1]
-    # move up tree
-    while curr:
-        if curr[-1] == '0':
-            curr = curr[:-1]
-        else:
-            curr = curr[:-1] + "0"
-            # move down tree
-            while curr not in places:
-                curr += "1"
-            places[curr] += places[left]
-            return
-    return
+def findLeftmost(places):
+    curr = '0'
+    while curr not in places:
+        curr += '0'
+    return curr
 
-def followUpRight(places, right):
-    curr = right[:-1]
+def findLeft(places, left):
+    curr = left[:-1]
+    if curr == '0':
+        # move over and down tree
+        l = left[:-1] + '0'
+        while l not in places:
+            l += '1'
+        return l
     # move up tree
-    while curr:
-        if curr[-1] == '1':
-            curr = curr[:-1]
+    l = left[:-1]
+    while l:
+        if l[-1] == '0':
+            l = l[:-1]
         else:
-            curr = curr[:-1] + "1"
+            l = l[:-1] + '0'
             # move down tree
-            while curr not in places:
-                curr += "0"
-            places[curr] += places[right]
-            return
-    return
+            while l not in places:
+                l += '1'
+            return l
+    return None
+
+def findRight(places, right):
+    curr = right[-1]
+    if curr == '0':
+        # move over and down tree
+        r = right[:-1] + '1'
+        while r not in places:
+            r += '0'
+        return r
+    # move up tree
+    r = right[:-1]
+    while r:
+        if r[-1] == '1':
+            r = r[:-1]
+        else:
+            r = r[:-1] + '1'
+            # move down tree
+            while r not in places:
+                r += '0'
+            return r
+    return None
 
 def explode(places, position):
-    #if position[-2] == '1' and position[:-2]+"01":
-    #    return True
-    if position[-1] == "0":
-        left = position
-        right = position[:-1] + "1"
-    else:
-        right = position
-        left = position[:-1] + "0"
+    left = position
+    right = position[:-1] + "1"
 
-    oldPlaces = deepcopy(places)
+    # nextLeft is the position of the next value to the left
+    nextLeft = findLeft(places, left)
+    # nextRight is the position of the next value to the left
+    nextRight = findRight(places, right)
 
     # solve left
+    # immediateLeft is the position of explodings pair value at the same level in the tree
     immediateLeft = left[:-2] + "0"
-    if immediateLeft in oldPlaces:
+    # if we have a regular number pair add left value to it
+    if immediateLeft in places:
         places[immediateLeft] += places[left]
-        #places[immediateLeft[:-1] + '1'] = 0
+    # Otherwise, set it to 0
     else:
-        followUpLeft(places, left)
-        #places[immediateLeft] = 0
-        if left[-2] == "0":
-            places[immediateLeft] = 0
+        places[immediateLeft] = 0
+        # if there is a value to the left, change it
+        if nextLeft:
+            places[nextLeft] += places[left]
 
     # solve right
+    # immediateRight is the position of explodings pair value at the same level in the tree
     immediateRight = right[:-2] + "1"
-    if immediateRight in oldPlaces:
+    # if we have a regular number pair add right value to it
+    if immediateRight in places:
         places[immediateRight] += places[right]
-        #places[immediateRight[:-1] + '0'] = 0
+    # Otherwise, set it to 0
     else:
-        followUpRight(places, right)
-        if right[-2] == "1":
+        if not nextRight:
             places[immediateRight] = 0
+        elif len(nextRight) != 5:
+            places[immediateRight] = 0
+        else:
+            if nextRight[:3] != right[:3]:
+                places[immediateRight] = 0
+        # if there is a value to the right, change it
+        if nextRight:
+            places[nextRight] += places[right]
 
     places.pop(left)
     places.pop(right)
-    return False
+
+    return nextRight
 
 def split(places, position):
     places[position + "0"] = places[position]//2
@@ -143,65 +170,89 @@ def sortKeys(places, newPlaces, curr):
     if len(newCurr) < 5:
         sortKeys(places, newPlaces, newCurr)
 
+def cleanLine(places):
+    modified = True
+    while modified:
+        p = findLeftmost(places)
+        modified = False
+        # while p is not the most rightmost element
+        while True:
+            if len(p) == 5:
+                tmp = places[p]
+                p = explode(places, p)
+                modified = True
+                if not p:
+                    break
+            else:
+                p = findRight(places, p)
+                if not p:
+                    break
+        p = findLeftmost(places)
+        while True:
+            if type(places[p]) == type(0) and places[p] > 9:
+                tmp = places[p]
+                split(places, p)
+                modified = True
+                break
+            p = findRight(places, p)
+            if not p:
+                break
 
 places = {}
-position = ""
 curr = json.loads(lines.pop(0))
-fillPlaces(places, position, curr)
-for line in lines:
-    while True:
-        modified = False
-        for p in places:
-            if len(p) == 5:
-                if p[-2] == "1" and p[:-2]+"01" in places:
-                    explode(places, p[:-2]+"01")
-                else:
-                    explode(places, p)
-                printHomework(places)
-                modified = True
-                break
-        if modified:
-            continue
-        modified = False
-        for p in places:
-            if type(places[p]) == type(0) and places[p] > 9:
-                split(places, p)
-                printHomework(places)
-                modified = True
-                break
-        if not modified:
-            break
-    
-    # add new line
-    nextPlaces = {}
-    nextPosition = ""
-    next = json.loads(line)
-    fillPlaces(nextPlaces, nextPosition, next)
-    printHomework(places)
-    places = addition(places, nextPlaces)
-    printHomework(places)
-    
-while True:
-    modified = False
-    for p in places:
-        if len(p) == 5:
-            if p[-2] == "1" and p[:-2]+"01" in places:
-                explode(places, p[:-2]+"01")
-            else:
-                explode(places, p)
-            #printHomework(places)
-            modified = True
-            break
-    if modified:
-        continue
-    modified = False
-    for p in places:
-        if type(places[p]) == type(0) and places[p] > 9:
-            split(places, p)
-            #printHomework(places)
-            modified = True
-            break
-    if not modified:
-        break
+fillPlaces(places, "", curr)
+cleanLine(places)
 
-printHomework(places)
+for line in lines:
+    # add new line
+    nPlaces = {}
+    next = json.loads(line)
+    fillPlaces(nPlaces, "", next)
+    places = addition(places, nPlaces)
+
+    # clean up math problem
+    cleanLine(places)
+
+def magnitude(places):
+    depth = 4
+    while depth >= 1:
+        nplaces = deepcopy(places)
+        for p in places:
+            if len(p) == depth and p in nplaces:
+                if p[-1] == '0':
+                    other = p[:-1] + '1'
+                    nplaces[p[:-1]] = places[p]*3 + places[other]*2
+                else:
+                    other = p[:-1] + '0'
+                    nplaces[p[:-1]] = places[other]*3 + places[p]*2
+                nplaces.pop(p)
+                nplaces.pop(other)
+        places = nplaces
+        depth -= 1
+
+    return places['']
+
+print(magnitude(places))
+
+sums = []
+# Gold
+for i in range(len(lines)):
+    for j in range(i+1, len(lines)):
+            
+        places = {}
+        curr = json.loads(lines[i])
+        fillPlaces(places, "", curr)
+        cleanLine(places)
+
+        # add new line
+        nPlaces = {}
+        next = json.loads(lines[j])
+        fillPlaces(nPlaces, "", next)
+        places = addition(places, nPlaces)
+
+        # clean up math problem
+        cleanLine(places)
+
+        sums.append(magnitude(places))
+
+print(max(sums))
