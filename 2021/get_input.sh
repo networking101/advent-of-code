e7a02a42a6e8c3c73ad1e608c5a9d5df27301c63
networking101 <kowpak14@gmail.com>
2023-12-02 11:30:31 -0500

    2023 day 2

--- 2021/get_input.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Provide day as argument."
-    echo "$0 N"
-    exit 0
-fi
-curl "https://adventofcode.com/2021/day/$1/input" -H 'Cookie: session=53616c7465645f5fa0c7d6dc806aeda466ed062c948200fca643fa39d43e7c4bb20af533d44fe640b48b570b50d0755e;' -o "./$1/input"
--- 2023/get_input.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Provide day as argument."
-    echo "$0 N"
-    exit 0
-fi
-curl "https://adventofcode.com/2023/day/$1/input" -H 'Cookie: session=53616c7465645f5f34db99e7553fdd2e7a5ff592d6f2c8eda080a0d3a96c5bfc27bab97552eb4b5a42eff0ed91e22de45e130bbdcfc34a30775c41a2d3b873a5;' -o "./$1/input"
