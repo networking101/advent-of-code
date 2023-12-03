fe7824885f15bdf19630d608e93e396b6fc23030
networking101 <kowpak14@gmail.com>
2023-12-03 14:26:20 -0500

    2023 day 3

--- 2023/3/3.py
+++ 2023/3/3.py
@@ -1,147 +1,74 @@
 with open("input", "r") as fp:
     lines = [line.strip() for line in fp]
 
-ysize = len(lines)
-xsize = len(lines[0])
-
-collection = []
+y_max = len(lines)
+x_max = len(lines[0])
 
 grid = []
+numbers = []
+
 for line in lines:
     tmp = []
-    for l in line:
-        tmp.append(l)
+    for col in line:
+        tmp.append(col)
     grid.append(tmp)
 
-def getNum(y, x):
-    xmin = x
-    xmax = x
-    number = []
-    number.append(grid[y][x])
-    if x-1 >= 0 and grid[y][x-1].isnumeric():
-        number.insert(0, grid[y][x-1])
-        xmin = x-1
+for y, row in enumerate(grid):
+    part_num = ''
+    xmin = -1
+    for x, c in enumerate(row):
+        if c.isnumeric():
+            if part_num == '':
+                xmin = x
+            part_num += c
+            if x == x_max - 1 or not grid[y][x+1].isnumeric():
+                numbers.append((int(part_num), xmin, x, y))
+                part_num = ''
+                xmin = -1
 
-        if x-2 >= 0 and grid[y][x-2].isnumeric():
-            number.insert(0, grid[y][x-2])
-            xmin = x-2
+gears = {}
 
-    if x+1 <=xsize and grid[y][x+1].isnumeric():
-        number.append(grid[y][x+1])
-        xmax = x+1
-
-        if x+2 <=xsize and grid[y][x+2].isnumeric():
-            number.append(grid[y][x+2])
-            xmax = x+2
-
-    num_int = int(''.join(number))
-
-    if (xmin, xmax, y) not in (collection):
-        # print(num_int)
-        collection.append((xmin, xmax, y))
-
-        return (int(num_int))
-    return 0
-
-def getNumStar(collection2, y, x):
-    xmin = x
-    xmax = x
-    number = []
-    number.append(grid[y][x])
-    if x-1 >= 0 and grid[y][x-1].isnumeric():
-        number.insert(0, grid[y][x-1])
-        xmin = x-1
-
-        if x-2 >= 0 and grid[y][x-2].isnumeric():
-            number.insert(0, grid[y][x-2])
-            xmin = x-2
-
-    if x+1 <=xsize and grid[y][x+1].isnumeric():
-        number.append(grid[y][x+1])
-        xmax = x+1
-
-        if x+2 <=xsize and grid[y][x+2].isnumeric():
-            number.append(grid[y][x+2])
-            xmax = x+2
-
-    num_int = int(''.join(number))
-    if (xmin, xmax, y) not in (collection2):
-        # print(num_int)
-        # print(xmin, xmax)
-        collection2.append((xmin, xmax, y))
-
-        return(int(num_int))
+def checkSymbol(y, x, part_num):
+    if x < 0 or y < 0 or x >= x_max or y >= y_max:
+        return False
     
-    return -1
+    if grid[y][x].isnumeric() or grid[y][x] == '.':
+        return False
+    
+    if grid[y][x] == '*':
+        if (y, x) not in gears:
+            gears[(y, x)] = [part_num]
+        else:
+            gears[(y, x)].append(part_num)
 
+    return True
 
-tot = 0
-tot2 = 0
+count1 = 0
+count2 = 0
+symbol = False
+for n in numbers:
+    part_num, xminval, xmaxval, y = n
+    for x in range(xminval, xmaxval+1):
+        if checkSymbol(y-1, x, part_num) or checkSymbol(y+1, x, part_num):
+            symbol = True
 
-for j, row in enumerate(grid):
-    for i, c in enumerate(row):
-        tot_list = []
-        if c != '.' and not c.isnumeric():
-            if grid[j-1][i-1].isnumeric():
-                tot += getNum(j-1, i-1)
-            if grid[j-1][i].isnumeric():
-                tot += getNum(j-1, i)
-            if grid[j-1][i+1].isnumeric():
-                tot += getNum(j-1, i+1)
-            if grid[j][i-1].isnumeric():
-                tot += getNum(j, i-1)
-            if grid[j][i+1].isnumeric():
-                tot += getNum(j, i+1)
-            if grid[j+1][i-1].isnumeric():
-                tot += getNum(j+1, i-1)
-            if grid[j+1][i].isnumeric():
-                tot += getNum(j+1, i)
-            if grid[j+1][i+1].isnumeric():
-                tot += getNum(j+1, i+1)
-
-        tot_list = []
-        if c == '*':
-            c2 = []
-            if grid[j-1][i-1].isnumeric():
-                t = getNumStar(c2, j-1, i-1)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j-1][i].isnumeric():
-                t = getNumStar(c2, j-1, i)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j-1][i+1].isnumeric():
-                t = getNumStar(c2, j-1, i+1)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j][i-1].isnumeric():
-                t = getNumStar(c2, j, i-1)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j][i+1].isnumeric():
-                t = getNumStar(c2, j, i+1)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j+1][i-1].isnumeric():
-                t = getNumStar(c2, j+1, i-1)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j+1][i].isnumeric():
-                t = getNumStar(c2, j+1, i)
-                if t > 0:
-                    tot_list.append(t)
-            if grid[j+1][i+1].isnumeric():
-                t = getNumStar(c2, j+1, i+1)
-                if t > 0:
-                    tot_list.append(t)
+        if x == xminval:
+            if checkSymbol(y-1, x-1, part_num) or checkSymbol(y, x-1, part_num) or checkSymbol(y+1, x-1, part_num):
+                symbol = True
         
-            if len(tot_list) == 2:
-                tmp = 1
-                # print(tot_list)
-                for i in tot_list:
-                    tmp *= i
-                tot2 += tmp
-            
+        if x == xmaxval:
+            if checkSymbol(y-1, x+1, part_num) or checkSymbol(y, x+1, part_num) or checkSymbol(y+1, x+1, part_num):
+                symbol = True
+        
+    if symbol:
+        count1 += part_num
 
-print(tot)
-print(tot2)
+    symbol = False
+
+print(count1)
+
+for k, g in gears.items():
+    if len(g) == 2:
+        count2 += g[0] * g[1]
+
+print(count2)
